16a8c5e8e7ec44e86d73dd2365d23cdde84cfc35
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-11-03 13:25:55 -0400

    Song new and create, song#new view

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,4 +2,24 @@ class SongsController < ApplicationController
   def index
     @songs = Song.all
   end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+  end
+
+  private
+
+     def song_params
+       params.require(:song).permit(
+        :title, 
+        :artist, 
+        :difficulty,
+        :lyrics,
+        )
+     end
+
 end
